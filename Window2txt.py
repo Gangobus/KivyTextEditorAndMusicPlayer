2f8eed4b046fbdfa65226722cc941db209ebf9cf
Sasha <sasha1900sasha@gmail.com>
2023-03-30 09:28:29 +0300

    30.03.2023 first commit, txt open save, input, differences

---
+++ Window2txt.py
@@ -0,0 +1,162 @@
+import os
+from kivy.uix.screenmanager import Screen
+from tkinter import Tk, filedialog
+from docx import Document
+from kivy.core.audio import SoundLoader
+from kivy.clock import Clock
+class Window2txt(Screen):
+    def on_start(self):
+        self.paused = True
+        self.current_pos = 0
+        self.song_time = 0
+        self.update_time = None
+        self.sound = None
+
+    def fps_check(self):
+        self.fps_monitor_start()
+
+    def choose_file(self, instance):
+        # открыть диалоговое окно выбора файла
+        try:
+            path = os.path.dirname(os.path.abspath(__file__))
+        except NameError:
+            path = os.getcwd()
+        file_name = self.file_chooser_dialog(path)
+        if file_name:
+            # загрузить звуковой файл
+            self.sound = SoundLoader.load(file_name)
+            self.song_time = 0
+
+    def file_chooser_dialog(self, path):
+        # открыть диалоговое окно выбора файла
+        try:
+            from tkinter import Tk
+            from tkinter.filedialog import askopenfilename
+        except ImportError:
+            return None
+        Tk().withdraw()
+        file_name = askopenfilename(initialdir=path,
+                                    title="Выберите MP3 файл",
+                                    filetypes=(("MP3 Files", "*.mp3"), ("All Files", "*.*")))
+
+    def play_pause(self, instance):
+        if self.sound:
+            if self.paused:
+                self.sound.play()
+                self.paused = False
+                self.update_time = Clock.schedule_interval(self.update_progress, 0.1)
+                instance.icon = "pause"
+            else:
+                self.sound.stop()
+                self.paused = True
+                Clock.unschedule(self.update_time)
+                instance.icon = "play"
+
+    def rewind(self, instance):
+        if self.sound:
+            self.current_pos = max(self.current_pos - 5, 0)
+            self.sound.seek(self.current_pos)
+
+    def fast_forward(self, instance):
+        if self.sound:
+            self.current_pos = min(self.current_pos + 5, self.sound.length)
+            self.sound.seek(self.current_pos)
+
+    def mute(self, instance):
+        if self.sound:
+            self.sound.volume = 0 if self.sound.volume > 0 else 1
+            instance.icon = "volume-off" if self.sound.volume == 0 else "volume-mute"
+
+    def update_progress(self, dt):
+        if self.sound:
+            self.current_pos = self.sound.get_pos()
+            self.ids.current_time.text = self.format_time(self.current_pos)
+            self.ids.progress.value = self.current_pos / self.sound.length
+
+    def format_time(self, seconds):
+        # helper method to format time in seconds to hh:mm:ss
+        return f"{int(seconds // 3600):02d}:{int((seconds // 60) % 60):02d}:{int(seconds % 60):02d}"
+
+    def open_txt_file_left_dialog(self, *args):
+        root = Tk()
+        root.withdraw()
+        file_path = filedialog.askopenfilename(filetypes=[("Text Files", "*.txt"), ("Word Documents", "*.docx")])
+        if file_path:
+            if file_path.endswith(".docx"):
+                document = Document(file_path)
+                self.ids.txt1.text = "\n".join([para.text for para in document.paragraphs])
+            else:
+                with open(file_path, 'r+', encoding='utf-8') as file:
+                    self.ids.txt1.text = file.read()
+
+    def save_left_text_to_file(self, *args):
+        file_path = filedialog.asksaveasfilename(defaultextension=".txt",
+                                                 filetypes=[("Text Files", "*.txt"), ("Word Documents", "*.docx")])
+        if file_path:
+            with open(file_path, 'w', encoding='utf-8') as file:
+                file.write(self.ids.txt1.text)
+                # Если файл имеет расширение .docx, то сохраняем его в формате docx
+                if file_path.endswith(".docx"):
+                    document = Document()
+                    document.add_paragraph(self.ids.txt1.text)
+                    document.save(file_path)
+
+    def open_txt_file_right_dialog(self, *args):
+        root = Tk()
+        root.withdraw()
+        file_path = filedialog.askopenfilename(filetypes=[("Text Files", "*.txt"), ("Word Documents", "*.docx")])
+        if file_path:
+            if file_path.endswith(".docx"):
+                document = Document(file_path)
+                self.ids.txt2.text = "\n".join([para.text for para in document.paragraphs])
+            else:
+                with open(file_path, 'r+', encoding='utf-8') as file:
+                    self.ids.txt2.text = file.read()
+
+    def save_right_text_to_file(self, *args):
+        file_path = filedialog.asksaveasfilename(defaultextension=".txt",
+                                                 filetypes=[("Text Files", "*.txt"), ("Word Documents", "*.docx")])
+        if file_path:
+            with open(file_path, 'w', encoding='utf-8') as file:
+                file.write(self.ids.txt1.text)
+
+                # Если файл имеет расширение .docx, то сохраняем его в формате docx
+                if file_path.endswith(".docx"):
+                    document = Document()
+                    document.add_paragraph(self.ids.txt1.text)
+                    document.save(file_path)
+
+    def find_differences(self):
+        text1 = self.ids.txt1.text
+        text2 = self.ids.txt2.text
+
+        # Split the texts into lists of words
+        words1 = text1.split()
+        words2 = text2.split()
+
+        # Find the differences between the two lists
+        diff = list(set(words2) - set(words1))
+
+        # Highlight the differences in the second text
+        for word in diff:
+            start_pos = text2.find(word)
+            end_pos = start_pos + len(word)
+            if start_pos != -1:
+                text2 = text2[:start_pos] + f"[|]{word}[|]" + text2[end_pos:]
+
+        self.ids.txt2.text = text2
+
+    def remove_differences(self):
+        text2 = self.ids.txt2.text
+        new_text2 = text2.replace('[|]', '')
+        self.ids.txt2.text = new_text2
+
+    # метод для увеличения размера шрифта
+    def increase_font_size1(self):
+        self.ids.txt1.font_size += 2
+        self.ids.txt2.font_size += 2
+
+    # метод для уменьшения размера шрифта
+    def decrease_font_size1(self):
+        self.ids.txt1.font_size -= 2
+        self.ids.txt2.font_size -= 2
---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+from kivy.core.audio import SoundLoader
+from kivy.clock import Clock
+from tkinter import Tk, filedialog
+from docx import Document
+from kivymd.uix.button import MDIconButton
+from kivy.uix.textinput import TextInput
+from kivy.properties import BooleanProperty
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivy.core.text import LabelBase
+from kivy.uix.screenmanager import Screen, ScreenManager
+from Window1txt import Window1txt
+from Window2txt import Window2txt
+
+class MainApp(MDApp):
+    def build(self):
+        Window.size = [1600, 800]
+        LabelBase.register(name='TimesNewRoman',
+                           fn_regular='times-new-roman.ttf')
+        self.compare_mode = False
+        self.highlight_color = (255, 0, 0, 1)  # set highlight color
+
+        Builder.load_file("w2txt.kv")
+        Builder.load_file("w1txt.kv")
+
+        sm=ScreenManager()
+        sm.add_widget(Window1txt(name="S1T"))
+        sm.add_widget(Window2txt(name="S2T"))
+        return sm
+MainApp().run()
